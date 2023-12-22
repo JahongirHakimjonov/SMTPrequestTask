cd35014ed6a3f429581177c4b66a644fa51079d7
JahongirHakimjonov <jahongirhakimjonov@gmail.com>
2023-12-22 18:23:24 +0500

    Strong and faster version

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import asyncio
 import csv
 import logging
 import os
-import smtplib
-import threading
 from email.message import EmailMessage
 
-import requests
+import aiofiles
+import aiohttp
+from aiosmtplib import SMTP
 
 from passwords import password
 
@@ -13,31 +14,40 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def download_images():
+async def download_image(session, gender, i):
+    folder_path = "peoples"
+    os.makedirs(folder_path, exist_ok=True)
+
+    image_url = f"https://randomuser.me/api/portraits/{gender}/{i}.jpg"
+    image_filename = os.path.join(folder_path, f"{gender}_{i}.jpg")
+
+    try:
+        async with session.get(image_url) as response:
+            response.raise_for_status()
+            image_content = await response.read()
+
+            async with aiofiles.open(image_filename, "wb") as image_file:
+                await image_file.write(image_content)
+
+            logger.info(f"{gender} {i} rasm saqlandi.")
+    except aiohttp.ClientError as e:
+        logger.error(f"{gender} {i} rasmni yuklab bolmadi. Xatolik: {e}")
+
+
+async def download_images():
     folder_path = "peoples"
     os.makedirs(folder_path, exist_ok=True)
 
     genders = ["men", "women"]
 
-    for gender in genders:
-        for i in range(1, 101):
-            image_url = f"https://randomuser.me/api/portraits/{gender}/{i}.jpg"
-            image_filename = f"{folder_path}/{gender}_{i}.jpg"
-
-            try:
-                response = requests.get(image_url)
-                response.raise_for_status()
-
-                with open(image_filename, "wb") as image_file:
-                    image_file.write(response.content)
-                logger.info(f"{gender} {i} rasm saqlandi.")
-            except requests.exceptions.RequestException as e:
-                logger.error(f"{gender} {i} rasmni yuklab bolmadi. Xatolik: {e}")
+    async with aiohttp.ClientSession() as session:
+        tasks = [asyncio.create_task(download_image(session, gender, i)) for gender in genders for i in range(1, 100)]
+        await asyncio.gather(*tasks)
 
     logger.info("Barcha rasmlar yuklandi.")
 
 
-def send_email_function(send_email, receiver_emails, password, subject, content_urls):
+async def send_email_function(send_email, receiver_emails, password, subject, content_urls):
     server = "smtp.gmail.com"
     msg = EmailMessage()
     msg["From"] = send_email
@@ -46,39 +56,44 @@ def send_email_function(send_email, receiver_emails, password, subject, content_
 
     for file in content_urls:
         with open(file, "rb") as f:
-            msg.add_attachment(f.read(), maintype="image", subtype="jpeg", filename=f.name.split("/")[-1])
+            file_content = f.read()
+            msg.add_attachment(file_content, maintype="image", subtype="jpeg", filename=os.path.basename(file))
 
-    with smtplib.SMTP_SSL(server, 465) as smtp_server:
-        smtp_server.login(send_email, password)
-        smtp_server.send_message(msg)
+    async with SMTP(hostname=server, port=465, use_tls=True) as smtp_server:
+        await smtp_server.login(send_email, password)
+        await smtp_server.send_message(msg)
         logger.info("Xabar jo'natildi :) {0}".format(" ".join(receiver_emails)))
 
 
-def send_email():
+async def send_email():
     os.chdir("peoples")
 
     images = [os.path.abspath(file) for file in os.listdir()]
 
     os.chdir("..")
 
-    with open("emails.csv", "r") as f:
-        reader = csv.DictReader(f)
-        emails = [row['emails'].strip() for row in reader]
+    try:
+        async with aiofiles.open("emails.csv", "r") as f:
+            content = await f.read()
+            reader = csv.DictReader(content.splitlines())
+            emails = [row['emails'].strip() for row in reader]
 
-    send_email_function(
-        send_email="hakimjonovjahongir0@gmail.com",
-        password=password,
-        receiver_emails=emails,
-        subject="Yangi rasmlar",
-        content_urls=images
-    )
+        await send_email_function(
+            send_email="hakimjonovjahongir0@gmail.com",
+            password=password,
+            receiver_emails=emails,
+            subject="Yangi rasmlar",
+            content_urls=images
+        )
+    except FileNotFoundError:
+        logger.error("Emails file not found.")
+    except Exception as e:
+        logger.exception("An unexpected error occurred while reading the emails file: %s", e)
 
 
 if __name__ == '__main__':
     try:
-        thread = threading.Thread(target=download_images)
-        thread.start()
-        thread.join()
-        send_email()
+        asyncio.run(download_images())
+        asyncio.run(send_email())
     except Exception as e:
         logger.exception("An unexpected error occurred: %s", e)
